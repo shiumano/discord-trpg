f6661ddb762d3481cd113ff935f7e5559d2ab2a3
shiumano <takkunmail2005@gmail.com>
2021-06-28 14:47:11 +0900

    アップデート機能の実装

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from discord.ext import commands
+import asyncio
+import async_timeout
+import importlib
+import restart
 
 version = 'a0.0.1'
 
@@ -9,8 +13,21 @@ async def on_ready():
     print('Login as {}'.format(bot.user))
 
 @bot.command()
+async def update(ctx,filename):
+    async with asyncio.ClientSession() as session:
+        with async_timeout.timeout(30):
+            async with session.get('https://raw.githubusercontent.com/shiumano/discord-trpg/master/{}'.format(filename)) as response:
+                code = response.text()
+    with open(filename,mode='w') as file:
+        file.write(code)
+
+    if file == 'main.py':
+        restart.rrstart_program()
+		
+
+@bot.command()
 async def version(ctx):
-    await ctx.send('version:{}'.format(version)})
+    await ctx.send('version:{}'.format(version))
 
 
 with open('../token') as file:
